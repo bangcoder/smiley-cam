6eede781d38feea12363240ef07f293235ea9231
SUNG HOON BANG <sunghoonbang@SUNGui-MacBookPro.local>
2019-08-22 07:44:47 +0900

    Complete 1

--- App.js
+++ App.js
@@ -1,19 +1,175 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  TouchableOpacity,
+  Platform
+} from "react-native";
+import * as Permissions from "expo-permissions";
+import { Camera } from "expo-camera";
+import * as FaceDetector from "expo-face-detector";
+import * as FileSystem from "expo-file-system";
+import * as MediaLibrary from "expo-media-library";
+import styled from "styled-components";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const { width, height } = Dimensions.get("window");
+const ALBUM_NAME = "Smiley Cam";
+console.log(width, height);
+
+const CenterView = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: cornflowerblue;
+`;
+
+const Text = styled.Text`
+  color: white;
+  font-size: 22px;
+`;
+
+const IconBar = styled.View`
+  margin-top: 40px;
+`;
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasPermission: null,
+      cameraType: Camera.Constants.Type.front,
+      smileDetected: false
+    };
+    this.cameraRef = React.createRef();
+  }
+
+  componentDidMount = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    // console.log(status);
+    if (status === "granted") {
+      this.setState({ hasPermission: true });
+    } else {
+      this.setState({ hasPermission: false });
+    }
+  };
+  render() {
+    const { hasPermission, cameraType, smileDetected } = this.state;
+    if (hasPermission === true) {
+      return (
+        <CenterView>
+          <Camera
+            style={{
+              width: width - 40,
+              height: height / 1.5,
+              borderRadius: 10,
+              overflow: "hidden"
+            }}
+            type={cameraType}
+            onFacesDetected={smileDetected ? null : this.onFacesDetected}
+            faceDetectorSettings={{
+              detectLandmarks: FaceDetector.Constants.Landmarks.all,
+              runClassifications: FaceDetector.Constants.Classifications.all
+            }}
+            ref={this.cameraRef}
+          />
+          <IconBar>
+            <TouchableOpacity onPress={this.switchCameraType}>
+              <MaterialIcons
+                name={
+                  cameraType === Camera.Constants.Type.front
+                    ? "camera-rear"
+                    : "camera-front"
+                }
+                size={50}
+                color="white"
+              />
+            </TouchableOpacity>
+          </IconBar>
+        </CenterView>
+      );
+    } else if (hasPermission === false) {
+      return (
+        <CenterView>
+          <Text>Don't have permission</Text>
+        </CenterView>
+      );
+    } else {
+      return (
+        <CenterView>
+          <ActivityIndicator />
+        </CenterView>
+      );
+    }
+  }
+  switchCameraType = () => {
+    const { cameraType } = this.state;
+    if (cameraType === Camera.Constants.Type.front) {
+      this.setState({
+        cameraType: Camera.Constants.Type.back
+      });
+    } else {
+      this.setState({
+        cameraType: Camera.Constants.Type.front
+      });
+    }
+  };
+  onFacesDetected = ({ faces }) => {
+    const face = faces[0];
+    if (face) {
+      if (face.smilingProbability > 0.7) {
+        this.setState({
+          smileDetected: true
+        });
+        this.takePhoto();
+        console.log("Cheeze");
+      } else {
+        console.log("sad");
+      }
+    }
+  };
+  takePhoto = async () => {
+    try {
+      if (this.cameraRef.current) {
+        let { uri } = await this.cameraRef.current.takePictureAsync({
+          quality: 1,
+          exif: true
+        });
+        if (uri) {
+          this.savePhoto(uri);
+        }
+      }
+    } catch (error) {
+      alert(error);
+      this.setState({
+        smileDetected: false
+      });
+      console.log(error);
+    }
+  };
+  savePhoto = async uri => {
+    try {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status === "granted") {
+        const asset = await MediaLibrary.createAssetAsync(uri);
+        let album = await MediaLibrary.getAlbumAsync(ALBUM_NAME);
+        if (album === null) {
+          album = await MediaLibrary.createAlbumAsync(ALBUM_NAME, asset);
+        } else {
+          await MediaLibrary.addAssetsToAlbumAsync([asset], album.id);
+        }
+        setTimeout(() => this.setState({ smileDetected: false }), 2000);
+
+        // console.log(album);
+        // console.log(asset);
+      } else {
+        this.setState({ hasPermission: false });
+      }
+      // console.log(status);
+      // console.log(uri);
+    } catch (error) {
+      // console.log("error");
+      console.log(error);
+    }
+  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
